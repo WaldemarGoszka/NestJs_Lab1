c74de6cad36a3c90ece59cb1c61d7aa961e6434d
WaldemarGoszka <goszka.waldemar@gmail.com>
2024-05-25 22:52:11 +0200

    create some PUT, POST, DELETE endpoints and file for REST extension

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('songs')
 export class SongsController {
+  @Post
+  create() {
+    return 'create a song endpoint';
+  }
   @Get
   findAll() {
     return 'find all songs';
+  }
+  @Get(':id')
+  findOne() {
+    return 'fetch songs on the based on id';
+  }
+  @Put(':id')
+  update() {
+    return 'update songs on the based on id';
+  }
+  @Delete(':id')
+  delete() {
+    return 'delete songs on the based on id';
   }
 }
